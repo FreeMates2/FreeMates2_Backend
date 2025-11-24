b5f7f189c23a177d03203d8737f120dc59c4a30b
lulyulalla <daye200@naver.com>
2025-11-24 19:04:56 +0900

    build(기본):기본세팅

---
+++ src/main/java/project/freemates2/FreeMates2Application.java
@@ -0,0 +1,13 @@
+package project.freemates2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeMates2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FreeMates2Application.class, args);
+  }
+
+}
---
+++ src/test/java/project/freemates2/FreeMates2ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.freemates2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeMates2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
