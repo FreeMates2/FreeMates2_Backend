c6ec9c8ff19da0482c2ebe987a0f21d8b99d92f7
lulyulalla <daye200@naver.com>
2025-12-09 16:37:29 +0900

    fix(import): requestbody수정

--- src/main/java/project/freemates2/api/user/presentation/UserController.java
+++ src/main/java/project/freemates2/api/user/presentation/UserController.java
@@ -1,10 +1,11 @@
 package project.freemates2.api.user.presentation;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.freemates2.api.user.application.UserService;
